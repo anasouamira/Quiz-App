49356771eb44d2dd4e4dd9f6e0844231425ac6cd
Ouamira <anasouamira@gmail.com>
2025-11-17 15:41:54 +0100

    Created By Anas Ouamira

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -1,4 +1,25 @@
 package com.example.quizapp
 
+import java.util.ArrayList
+
 object Constants {
+
+    fun getQuestions() : ArrayList<Question>{
+
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,
+            "What country does this flag belong to ?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "",
+            "",
+            "",
+
+        )
+
+
+
+    }
 }
