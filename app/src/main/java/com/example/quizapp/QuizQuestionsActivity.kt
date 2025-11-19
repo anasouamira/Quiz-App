646fc054f2255c39720ab1522c22c323afacb56e
Ouamira <anasouamira@gmail.com>
2025-11-19 10:30:56 +0100

    Created By Anas Ouamira

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,28 +1,40 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var progressBar : ProgressBar? = null
-    private var tvProcess : TextView? = null
-    private var tvQuestion : TextView? = null
-    private var ivImage : ImageView? = null
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
-    private var tvOptionOne : TextView? = null
-    private var tvOptionTwo : TextView? = null
-    private var tvOptionThree : TextView? = null
-    private var tvOptionFour : TextView? = null
+
+    private var progressBar: ProgressBar? = null
+    private var tvProcess: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var btnSubmit : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +43,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         val controller = WindowInsetsControllerCompat(window, window.decorView)
         controller.hide(WindowInsetsCompat.Type.systemBars())
-        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 
         progressBar = findViewById(R.id.progressBar)
         tvProcess = findViewById(R.id.tv_progress)
@@ -43,14 +56,25 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionThree = findViewById(R.id.tv_option_Three)
         tvOptionFour = findViewById(R.id.tv_option_Four)
 
+        btnSubmit = findViewById(R.id.btnSubmit)
 
 
-        val questionList = Constants.getQuestions()
-        var currentPosition = 1
+        tvOptionOne?.setOnClickListener  (this)
+        tvOptionTwo?.setOnClickListener (this)
+        tvOptionThree?.setOnClickListener (this)
+        tvOptionFour?.setOnClickListener (this)
 
-        val question : Question = questionList[currentPosition-1]
-        progressBar?.progress = currentPosition
-        tvProcess?.text = "${currentPosition}/${progressBar?.max}"
+
+        mQuestionList = Constants.getQuestions()
+        setQuestion()
+
+    }
+
+    private fun setQuestion(){
+
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
+        progressBar?.progress = mCurrentPosition
+        tvProcess?.text = "${mCurrentPosition}/${progressBar?.max}"
         tvQuestion?.text = question.question
         ivImage?.setImageResource(question.image)
 
@@ -59,11 +83,75 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionThree?.text = question.optionTree
         tvOptionFour?.text = question.optionFour
 
+        if(mCurrentPosition == mQuestionList!!.size){
+            btnSubmit?.text = "FINISH"
+        }else{
+            btnSubmit?.text = "NEXT"
+        }
+    }
 
+    private fun defaultOptionView(){
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0,it)
+        }
+        tvOptionTwo?.let {
+            options.add(1,it)
+        }
+        tvOptionThree?.let {
+            options.add(2,it)
+        }
+        tvOptionFour?.let {
+            options.add(3,it)
+        }
 
-
-
-
+        for (option in options){
+            option.setTextColor(Color.BLACK)
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
 
     }
+    private fun selectedOptionView(tv : TextView, selectedOptionNum : Int){
+        defaultOptionView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.BLUE)
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.tv_option_One ->{
+                tvOptionOne?.let {
+                    selectedOptionView(it,1)
+                }
+            }
+            R.id.tv_option_Two ->{
+                tvOptionTwo?.let {
+                    selectedOptionView(it,2)
+                }
+            }
+            R.id.tv_option_Three ->{
+                tvOptionThree?.let {
+                    selectedOptionView(it,3)
+                }
+            }
+            R.id.tv_option_Four ->{
+                tvOptionFour?.let {
+                    selectedOptionView(it,4)
+                }
+            }
+
+        }
+    }
+
 }
